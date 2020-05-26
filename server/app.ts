e56d733b915c9cd118af5aedfb0121750626eecc
Dang Van Thanh <dangvanthanh@dangthanh.org>
2020-05-26 22:25:27 +0700

    Update client

--- server/app.ts
+++ server/app.ts
@@ -1,50 +1,15 @@
 import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
-
-// import {
-//   getProduct,
-//   addProduct,
-//   updateProduct,
-//   deleteProduct
-// } from './controllers/product.ts';
-
-// Router
-// router
-//   .get('/v1/getProduct', getProduct)
-//   .post('/v1/addProduct', addProduct)
-//   .patch('/v1/updateProduct', updateProduct)
-//   .delete('/v1/deleteProduct', deleteProduct);
-
-const products = new Map<string, any>();
-products.set('1', {
-  id: '1',
-  title: 'Shifty Boa White 2020',
-  description: 'Focused around comfort and flexibility'
-});
+import ProductController from './controllers/product.ts';
 
 const router = new Router();
 router
-  .get('/', context => {
-    context.response.body = 'Shopyyy';
-  })
-  .get('/products', context => {
-    context.response.body = Array.from(products.values());
-  })
-  .get('/product/:id', context => {
-    if (
-      context.params &&
-      context.params.id &&
-      products.has(context.params.id)
-    ) {
-      context.response.body = products.get(context.params.id);
-    }
-  });
+  .get('/products', ProductController.index)
+  .get('/product/:id', ProductController.show);
 
 // Starting the server
 const app = new Application();
 const PORT = 3000;
 app.use(router.routes());
 app.use(router.allowedMethods());
-
-console.log(`Listening on port ${PORT}`);
-
 await app.listen({ port: PORT });
+console.log(`Listening on port ${PORT}`);
--- server/controllers/product.ts
+++ server/controllers/product.ts
@@ -1,122 +1,43 @@
-import db from '../helpers/dbconnect.ts';
-const database = db.getDatabase;
-const products = database.collection('products');
+const products = new Map<string, any>();
+products.set('1', {
+  id: '1',
+  title: 'Shifty Boa White 2020',
+  description: 'Focused around comfort and flexibility',
+  price: 45.9,
+  thumbnail: ''
+});
 
-interface Product {
-  _id: {
-    $oid: string;
-  };
-  thumbnail: string;
-  title: string;
-  description: string;
-  price: number;
+products.set('22', {
+  id: '2',
+  title: 'Shifty Boa White 2020',
+  description: 'Focused around comfort and flexibility',
+  price: 75.9,
+  thumbnail: ''
+});
+
+products.set('3', {
+  id: '3',
+  title: 'Shifty Boa White 2020',
+  description: 'Focused around comfort and flexibility',
+  price: 95.9,
+  thumbnail: ''
+});
+
+class ProductController {
+  async index({ response }) {
+    response.body = Array.from(products.values());
+  }
+
+  async show({ params, response }) {
+    const id = params ? params.id : 1;
+    response.body = products.get(id);
+  }
+
+  // async create({ request, response }) {}
+
+  // async update({ params, request, response }) {}
+
+  // async delete({ params, response }) {}
 }
 
-export const getProducts: any = async (context: any) => {
-  try {
-    const data: any = await products.find();
-
-    if (data) {
-      context.response.body = data;
-      context.response.status = 200;
-    } else {
-      context.response.body = 'not found';
-      context.response.status = 204;
-    }
-  } catch (e) {
-    context.response.body = null;
-    context.response.status = 500;
-    console.log(e);
-  }
-};
-
-export const getProduct: any = async (context: any) => {
-  try {
-    let id: string = context.params.id;
-    const data: any = await products.findOne({ _id: { $oid: id } });
-
-    if (data) {
-      context.response.body = data;
-      context.response.status = 200;
-    } else {
-      context.response.body = 'not found';
-      context.response.status = 204;
-    }
-  } catch (e) {
-    context.response.body = null;
-    context.response.status = 500;
-    console.log(e);
-  }
-};
-
-export const addProduct = async (context: any) => {
-  try {
-    let body: any = await context.request.body();
-    const { thumbnail, name, description, price } = body.value;
-    const id = await products.insertOne({
-      thumbnail,
-      name,
-      description,
-      price
-    });
-
-    context.response.body = id;
-    context.response.status = 200;
-  } catch (e) {
-    context.response.body = null;
-    context.response.status = 500;
-    console.log(e);
-  }
-};
-
-export const updateProduct: any = async (context: any) => {
-  try {
-    const id: string = context.params.id;
-    let body: any = await context.request.body();
-
-    let data: {
-      thumbnail?: String;
-      name?: String;
-      description?: String;
-      price?: Number;
-    } = {};
-
-    if (body.value.thumbnail) {
-      data['thumbnail'] = body.value.thumbnail;
-    }
-    if (body.value.name) {
-      data['name'] = body.value.name;
-    }
-    if (body.value.description) {
-      data['description'] = body.value.description;
-    }
-    if (body.value.price) {
-      data['price'] = body.value.price;
-    }
-
-    const result = await products.updateOne(
-      { _id: { $oid: id } },
-      { $set: data }
-    );
-
-    context.response.body = result;
-    context.response.status = 200;
-  } catch (e) {
-    context.response.body = null;
-    context.response.status = 500;
-    console.log(e);
-  }
-};
-
-export const deleteProduct: any = async (context: any) => {
-  try {
-    let id: string = context.params.id;
-    const result = await products.deleteOne({ _id: { $oid: id } });
-    context.response.body = { result };
-    context.response.status = 200;
-  } catch (e) {
-    context.response.body = null;
-    context.response.status = 500;
-    console.log(e);
-  }
-};
+export default new ProductController();
