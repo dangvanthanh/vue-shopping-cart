2208e916cd70e02f4754aea6a971445d270c9405
Dang Van Thanh <dangvanthanh@dangthanh.org>
2020-08-24 21:48:42 +0700

    Update minimal layout for detail

--- client/public/assets/app.js
+++ client/public/assets/app.js
@@ -12199,86 +12199,6 @@
                     ])
                   ],
                   2
-                ),
-                _vm._v(" "),
-                _c(
-                  "span",
-                  {
-                    staticClass: "flex ml-3 pl-3 py-2 border-l-2 border-gray-200"
-                  },
-                  [
-                    _c("a", { staticClass: "text-gray-400" }, [
-                      _c(
-                        "svg",
-                        {
-                          staticClass: "w-5 h-5",
-                          attrs: {
-                            fill: "currentColor",
-                            "stroke-linecap": "round",
-                            "stroke-linejoin": "round",
-                            "stroke-width": "2",
-                            viewBox: "0 0 24 24"
-                          }
-                        },
-                        [
-                          _c("path", {
-                            attrs: {
-                              d:
-                                "M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"
-                            }
-                          })
-                        ]
-                      )
-                    ]),
-                    _vm._v(" "),
-                    _c("a", { staticClass: "ml-2 text-gray-400" }, [
-                      _c(
-                        "svg",
-                        {
-                          staticClass: "w-5 h-5",
-                          attrs: {
-                            fill: "currentColor",
-                            "stroke-linecap": "round",
-                            "stroke-linejoin": "round",
-                            "stroke-width": "2",
-                            viewBox: "0 0 24 24"
-                          }
-                        },
-                        [
-                          _c("path", {
-                            attrs: {
-                              d:
-                                "M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"
-                            }
-                          })
-                        ]
-                      )
-                    ]),
-                    _vm._v(" "),
-                    _c("a", { staticClass: "ml-2 text-gray-400" }, [
-                      _c(
-                        "svg",
-                        {
-                          staticClass: "w-5 h-5",
-                          attrs: {
-                            fill: "currentColor",
-                            "stroke-linecap": "round",
-                            "stroke-linejoin": "round",
-                            "stroke-width": "2",
-                            viewBox: "0 0 24 24"
-                          }
-                        },
-                        [
-                          _c("path", {
-                            attrs: {
-                              d:
-                                "M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"
-                            }
-                          })
-                        ]
-                      )
-                    ])
-                  ]
                 )
               ]),
               _vm._v(" "),
@@ -12288,18 +12208,18 @@
                 )
               ]),
               _vm._v(" "),
-              _c("div", { staticClass: "flex mt-6" }, [
+              _c("p", { staticClass: "mt-6" }, [
                 _c(
                   "span",
                   { staticClass: "font-semibold text-2xl text-orange" },
                   [_vm._v("$ " + _vm._s(_vm.product.price))]
-                ),
-                _vm._v(" "),
-                _c(
-                  "button",
-                  { staticClass: "flex ml-auto button button-primary px-6" },
-                  [_vm._v("\n          Add to cart\n        ")]
-                ),
+                )
+              ]),
+              _vm._v(" "),
+              _c("div", { staticClass: "flex mt-6" }, [
+                _c("button", { staticClass: "button button-primary px-6" }, [
+                  _vm._v("\n          Add to cart\n        ")
+                ]),
                 _vm._v(" "),
                 _c(
                   "button",
--- client/src/components/ProductDetail.vue
+++ client/src/components/ProductDetail.vue
@@ -32,62 +32,19 @@
               {{ product.rating > 1 ? 'Reviews' : 'Review' }}</span
             >
           </span>
-          <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
-            <a class="text-gray-400">
-              <svg
-                fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                class="w-5 h-5"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"
-                ></path>
-              </svg>
-            </a>
-            <a class="ml-2 text-gray-400">
-              <svg
-                fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                class="w-5 h-5"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"
-                ></path>
-              </svg>
-            </a>
-            <a class="ml-2 text-gray-400">
-              <svg
-                fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                class="w-5 h-5"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"
-                ></path>
-              </svg>
-            </a>
-          </span>
         </div>
         <p class="leading-relaxed">
           {{ product.description }}
         </p>
 
-        <div class="flex mt-6">
+        <p class="mt-6">
           <span class="font-semibold text-2xl text-orange"
             >$ {{ product.price }}</span
           >
-          <button
-            class="flex ml-auto button button-primary px-6"
-          >
+        </p>
+
+        <div class="flex mt-6">
+          <button class="button button-primary px-6">
             Add to cart
           </button>
           <button
@@ -119,7 +76,7 @@ export default {
   methods: {
     fillStar(n, rating) {
       return n <= rating ? 'currentColor' : 'none';
-    },
-  },
+    }
+  }
 };
 </script>
