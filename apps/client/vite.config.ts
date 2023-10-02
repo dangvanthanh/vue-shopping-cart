a88a20feae21458be3ffe374d3591d47a979a51d
Dang Van Thanh <dangvanthanh@dangthanh.org>
2023-10-02 09:12:40 +0700

    Update deno for fe

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -2,13 +2,15 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// NOTE(bartlomieju): this is a papercut that shouldn't be required, see README.md
+import "npm:vue";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      '@styled-system': path.resolve(__dirname, 'styled-system'),
     },
   },
 })
