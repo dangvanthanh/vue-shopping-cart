0c80de588264f78cebefb0e3919838feea7d80b4
Dang Van Thanh <dangvanthanh@dangthanh.org>
2024-04-02 19:54:31 +0700

    feat: remove hash in vue router

--- apps/web/src/router/index.ts
+++ apps/web/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const Home = () => import('../views/Home.vue')
 const Login = () => import('../views/Login.vue')
@@ -18,7 +18,7 @@ const routes = [
 ]
 
 const router = createRouter({
-	history: createWebHashHistory(),
+	history: createWebHistory(),
 	routes,
 	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 	scrollBehavior: (to: any) => {
